edf0f941c1582fb7bbe6a24daaf6d3e5fda5e59d
Lpq <1322325183@qq.com>
2024-05-25 23:50:11 +0800

    实现游戏首页到单机模式选择页面的跳转

--- app/src/main/java/com/example/aircraftwar2024/MainActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.aircraftwar2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    RadioGroup bgmGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start_button);
+        bgmGroup = findViewById(R.id.bgm_group);
+
+        startButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, OfflineActivity.class);
+            // TODO 应用 intent.putExtra 将音乐是否开启作为参数传递
+
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/aircraftwar2024/OfflineActivity.java
@@ -0,0 +1,14 @@
+package com.example.aircraftwar2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
