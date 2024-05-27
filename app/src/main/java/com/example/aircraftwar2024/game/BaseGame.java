4993fff9f51646bbbd9def5d3df9230c691497b7
Jin_zd <2295508804@qq.com>
2024-05-27 17:26:16 +0800

    修正文本框位置

--- app/src/main/java/com/example/aircraftwar2024/game/BaseGame.java
+++ app/src/main/java/com/example/aircraftwar2024/game/BaseGame.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -467,11 +468,13 @@ public abstract class BaseGame extends SurfaceView implements SurfaceHolder.Call
     }
 
     private void paintScoreAndLife() {
-        mPaint.setTextSize(20);
+        mPaint.setTextSize(80);
         mPaint.setColor(Color.RED);
-        canvas.drawText("Score:" + score, 0, 100, mPaint);
-        canvas.drawText("Life:" + heroAircraft.getHp(), 0, 100, mPaint);
-        
+        mPaint.setTypeface(Typeface.create("my", Typeface.BOLD_ITALIC));
+
+        canvas.drawText("Score:" + score, 10, 100, mPaint);
+        canvas.drawText("Life:" + heroAircraft.getHp(), 10, 180, mPaint);
+
     }
 
     @Override
