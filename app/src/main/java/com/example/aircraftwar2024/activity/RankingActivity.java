2b82efd5d2d2c957bcfe8fbfb62f76410fcda971
Lpq <1322325183@qq.com>
2024-06-03 21:29:34 +0800

    feat: 实现了删除数据的框架

--- app/src/main/java/com/example/aircraftwar2024/activity/RankingActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/activity/RankingActivity.java
@@ -1,10 +1,14 @@
 package com.example.aircraftwar2024.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.aircraftwar2024.R;
 
@@ -29,6 +33,19 @@ public class RankingActivity extends AppCompatActivity {
         list.setAdapter(simpleAdapter);
 
         // TODO: 删除弹窗
+        list.setOnItemClickListener((parent, view, position, id) -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(RankingActivity.this)
+                    .setTitle("提示")
+                    .setMessage("确定删除该条记录么")
+                    .setPositiveButton("确定", (dialog, which) -> {
+                        // TODO 删除一行数据 使用 position
+                        simpleAdapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("取消", (dialog, which) -> {})
+                    .create();
+            alertDialog.show();
+        });
+
 
         Button returnButton = findViewById(R.id.return_button);
         returnButton.setOnClickListener(view->{
