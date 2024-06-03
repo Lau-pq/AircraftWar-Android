cd15a3374d5218767c011db46a5d18e01464dab0
Jin_zd <2295508804@qq.com>
2024-06-03 17:36:04 +0800

    初步实现排行榜

---
+++ app/src/main/java/com/example/aircraftwar2024/DAO/DbOpenHelper.java
@@ -0,0 +1,23 @@
+package com.example.aircraftwar2024.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbOpenHelper extends SQLiteOpenHelper {
+    public DbOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/aircraftwar2024/activity/RankingActivity.java
@@ -0,0 +1,43 @@
+package com.example.aircraftwar2024.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.aircraftwar2024.R;
+
+import java.util.List;
+import java.util.Map;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ListView list = findViewById(R.id.list);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this,
+                getData(),
+                R.layout.activity_item,
+                new String[]{"排名", "用户名", "得分", "时间"},
+                new int[]{R.id.rank, R.id.name, R.id.score, R.id.time}
+        );
+
+        list.setAdapter(simpleAdapter);
+
+        // TODO: 删除弹窗
+
+        Button returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(view->{
+                    // TODO: 返回首页
+                }
+        );
+    }
+
+    private List<Map<String, Object>> getData() {
+        return null;
+    }
+}
