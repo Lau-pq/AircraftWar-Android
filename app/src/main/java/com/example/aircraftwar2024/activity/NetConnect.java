aed10ada91f986cb35d0846b0c64b6bf1fc16af9
Lpq <1322325183@qq.com>
2024-06-12 08:44:45 +0800

    fix: 修复了不运行的bug

---
+++ app/src/main/java/com/example/aircraftwar2024/activity/NetConnect.java
@@ -0,0 +1,78 @@
+package com.example.aircraftwar2024.activity;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.example.aircraftwar2024.aircraft.HeroAircraft;
+import com.example.aircraftwar2024.message.GameMessage;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class NetConnect implements Runnable{
+    private static final String TAG = "NetConnect";
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private Handler toCilentHandle;
+
+    public NetConnect(Handler handler) {
+        this.toCilentHandle = handler;
+    }
+    @Override
+    public void run() {
+        try {
+            socket = new Socket();
+            socket.connect(new InetSocketAddress
+                    ("10.0.2.2", 9999), 5000);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            writer = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8)), true);
+            Log.i(TAG, "connect to server");
+
+            // start enemy over
+            new Thread(() -> {
+                String fromserver;
+                try {
+                    while((fromserver = reader.readLine()) != null) {
+                        Message msg = new Message();
+                        switch (fromserver) {
+                            case "start" -> msg.what = GameMessage.start;
+                            case "over" -> msg.what = GameMessage.over;
+                            default -> msg.what = GameMessage.enemy;
+                        }
+                        msg.obj = fromserver;
+                        toCilentHandle.sendMessage(msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            try {
+                while (true) {
+                    Thread.sleep(50);
+                    int score = 0;
+                    writer.println("Score: " + score);
+                    if (HeroAircraft.getHeroAircraft().getHp() <= 0) {
+                        writer.println("Over");
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
